49e026773884d7733d0284d4cd0afa18e178f4a6
AnnkoATAMA <annkopan0903@gmail.com>
2025-01-18 12:26:13 +0900

    sqlいじっちゃった

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (username, password) VALUES
-    ('testuser', '5e884898da28047151d0e56f8dc6292773603d0d6aabbddfc73a27fb57e9e6f6'); -- パスワード= "password"
+    ('testuser', 'testuser');
--- sql/migrations/create_users_table.sql
+++ sql/migrations/create_users_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
                        id INT AUTO_INCREMENT PRIMARY KEY,
                        username VARCHAR(50) NOT NULL UNIQUE,
-                       password VARCHAR(64) NOT NULL, -- SHA-256ハッシュ格納用
+                       password VARCHAR(64) NOT NULL,
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
